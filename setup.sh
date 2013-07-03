08553afd6251294d30c9d7deaaa5f2f262307b59
Alec Clews <alecclews@gmail.com>
2013-07-03 22:34:41 +1000

    Added vim and removed dotfiles

--- setup.sh
+++ setup.sh
@@ -26,18 +26,20 @@ sudo apt-add-repository -y ppa:cassou/emacs
 sudo apt-get update
 sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
 
-# git pull and install dotfiles as well
-cd $HOME
-if [ -d ./dotfiles/ ]; then
-    mv dotfiles dotfiles.old
-fi
-if [ -d .emacs.d/ ]; then
-    mv .emacs.d .emacs.d~
-fi
-git clone https://github.com/startup-class/dotfiles.git
-ln -sb dotfiles/.screenrc .
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/.bashrc_custom .
-ln -sf dotfiles/.emacs.d .
+sudo apt-get install -y vim vim-addon-manager vim-scripts
 
+# # git pull and install dotfiles as well
+# cd $HOME
+# if [ -d ./dotfiles/ ]; then
+#     mv dotfiles dotfiles.old
+# fi
+# if [ -d .emacs.d/ ]; then
+#     mv .emacs.d .emacs.d~
+# fi
+# git clone https://github.com/startup-class/dotfiles.git
+# ln -sb dotfiles/.screenrc .
+# ln -sb dotfiles/.bash_profile .
+# ln -sb dotfiles/.bashrc .
+# ln -sb dotfiles/.bashrc_custom .
+# ln -sf dotfiles/.emacs.d .
+# 
